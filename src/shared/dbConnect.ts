ac0725bc7d77f6cf7fb56f342776035fc904c320
Ahmad Alawneh <aalawneh19@gmail.com>
2024-08-30 16:18:47 +0300

    initial docker setup

--- src/shared/dbConnect.ts
+++ src/shared/dbConnect.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connectToMongoDB() {
 	try {
-		await mongoose.connect('mongodb://localhost:27017/tasks');
+		await mongoose.connect('mongodb://mongo:27017/tasks');
 		console.log('Connected to MongoDB');
 	} catch (error) {
 		console.error('Error connecting to MongoDB', error);
